1bd14362e1fc7306e2d3223ccc137eeb8183a9ab
salilkansal <salilkansal.92@gmail.com>
2016-07-12 23:27:33 -0700

    Fixed some test cases. Passes all the test cases on the website.

--- save_the_prisoner.py
+++ save_the_prisoner.py
@@ -1,21 +1,12 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-test_cases = input()
 
 
 def get_last_sweet(num_of_prisoners, num_of_sweets, prisoner_id):
-    for x in range(num_of_sweets):
-        prisoner_id = get_next_prisoner(num_of_prisoners, prisoner_id)
-    return num_of_prisoners if prisoner_id == 1 else prisoner_id - 1
+    num_of_sweets %= num_of_prisoners
+    prisoner_id = (prisoner_id + num_of_sweets - 1) % num_of_prisoners
+    return num_of_prisoners if prisoner_id == 0 else prisoner_id
 
 
-def get_next_prisoner(num_of_prisoners, curr_prisoner):
-    return (curr_prisoner + 1) % num_of_prisoners
-
-
-for case in range(0, test_cases):
-    list = map(int, raw_input("Input").split())
-    num_of_prisoners = list[0]
-    num_of_sweets = list[1]
-    prisoner_id = list[2]
-    print list
-    print get_last_sweet(num_of_prisoners, num_of_sweets, prisoner_id)
+for case in range(input()):
+    n, s, p = map(int, raw_input().split())
+    print get_last_sweet(n, s, p)
